8a5e8f07875854c01c3a9f9f6ecdec4e45471fc4
Marcos <mpozzolo.dg@gmail.com>
2023-01-31 22:03:31 -0300

    ajustes finales

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 function mostrarMas () {
     let hobbiesContainer = document.getElementById ("hobbies-container")
+    let botonOcultar = document.getElementById ("btn-conocer-mas")
     hobbiesContainer.style.display = "block"
+    botonOcultar.style.display = "none"
 }
 
 let btnConocerMas = document.getElementById ("btn-conocer-mas")
